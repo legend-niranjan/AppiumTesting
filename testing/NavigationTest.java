5dd625c63b4c03f48ad0b576eff2b4ec5f47a308
legend-niranjan <143837826+legend-niranjan@users.noreply.github.com>
2025-02-07 18:00:35 +0530

    testing

---
+++ testing/NavigationTest.java
@@ -0,0 +1,64 @@
+package testing;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class NavigationTest {
+
+	public static void main(String[] args) {
+		 // Setup desired capabilities
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "motorola moto g85 5G");
+        caps.setCapability("appPackage", "com.example.calculatorapp");
+        caps.setCapability("appActivity", "com.example.calculatorapp.MainActivity");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+
+        try {
+            // Initialize the Appium driver
+        	 AppiumDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
+
+            // Perform addition operation
+        	 WebElement num1Field = driver.findElement(By.id("com.example.calculatorapp:id/etFirstNumber"));
+        	 System.out.println("NumField 1 : "+num1Field);
+              num1Field.sendKeys("5");
+
+              WebElement num2Field = driver.findElement(By.id("com.example.calculatorapp:id/etSecondNumber"));
+              num2Field.sendKeys("3");
+
+              WebElement addButton = driver.findElement(By.id("com.example.calculatorapp:id/btnAdd"));
+            addButton.click();
+
+            // Navigate to the second screen
+            WebElement navigateButton = driver.findElement(By.id("com.example.calculatorapp:id/btnNavigate"));
+            navigateButton.click();
+
+            WebElement resultView = driver.findElement(By.id("com.example.calculatorapp:id/textViewMessage"));
+            String resultText = resultView.getText();
+            System.out.println(" resultText :: "+resultText);
+            if(resultText.equalsIgnoreCase("Welcome to the Second Screen!")) {
+                System.out.println("Successfully navigated to SecondActivity!!!");
+            } else {
+                System.out.println("Not able to navigate!!!.");
+            }
+
+            
+            System.out.println("Navigation to the second screen was successful!");
+
+            driver.quit();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+	}
+
+}
---
+++ testing/VehicleInsuranceAppTest.java
@@ -0,0 +1,80 @@
+package testing;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class VehicleInsuranceAppTest {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+			capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "vivo V2334");
+			capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+			capabilities.setCapability("appPackage", "com.example.vehicleinsuranceapplication");
+			capabilities.setCapability("appActivity", ".SnapActivity");
+			
+			AppiumDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
+			
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			
+			WebElement loginButton = driver.findElement(By.id("btnlogin"));
+			loginButton.click();
+			
+			WebElement sinupText = driver.findElement(By.id("sinupText"));
+			sinupText.click();
+			
+			WebElement registerTitle = driver.findElement(By.xpath("//android.widget.TextView[@text='Register']"));
+			
+			if(registerTitle.getText().equals("Register")) {
+				System.out.println("Successfully navigated to Register Page");
+			}
+			
+			WebElement fullname = driver.findElement(By.xpath("//android.widget.EditText[@hint='Full Name']"));
+			WebElement registerUsername = driver.findElement(By.xpath("//android.widget.EditText[@hint='set username']"));
+			WebElement registerEmail = driver.findElement(By.xpath("//android.widget.EditText[@hint='Email Address']"));
+			WebElement registerPassword = driver.findElement(By.xpath("//android.widget.EditText[@hint='set password']"));
+			WebElement registerButton = driver.findElement(By.id("register"));
+			
+			fullname.sendKeys("Niranjan Suryawanshi");
+			registerUsername.sendKeys("Niranjan");
+			registerEmail.sendKeys("niranjansurywanshi318@gmail.com");
+			registerPassword.sendKeys("Niranjan123");
+			registerButton.click();
+			
+			System.out.println("Registration Successfull!");
+			
+			WebElement loginUsernameField = driver.findElement(By.xpath("//android.widget.EditText[@hint='username']"));
+			WebElement loginPasswordField = driver.findElement(By.xpath("//android.widget.EditText[@hint='password']"));
+			WebElement loginButtonElement = driver.findElement(By.id("Login"));
+			
+			loginUsernameField.sendKeys("Niranjan");
+			loginPasswordField.sendKeys("Niranjan123");
+			
+			loginButtonElement.click();
+			
+			WebElement startClaimButtonElement = driver.findElement(By.id("cardView"));
+			
+			if(startClaimButtonElement.isDisplayed()) {
+				System.out.println("Login successfull!");
+				System.out.println("Navigated to the DashboardActivity");
+			}
+			driver.quit();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		}
+
+}
